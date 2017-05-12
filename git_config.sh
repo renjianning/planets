56e0693d19a8adef119d8415be35aebacad6443f
renjianning <renjianning@gmail.com>
2017-05-11 22:14:46 -0700

    history of command line

---
+++ git_config.sh
@@ -0,0 +1,201 @@
+history | tail -7 | colrm 1 3
+history | tail -7 | colrm 1 4
+history | tail -7 | colrm 1 5
+history | tail -7 | colrm 1 6
+history | tail -7 | colrm 1 7
+nano test.txt
+bash test.txt *[AB].txt
+nano test.txt
+nano test.txt
+nano do-stats.sh
+bash do-stats.sh *[AB].txt
+nano do-stats.sh
+bash do-stats.sh *[AB].txt
+ls
+cd ..
+ls
+ls -F
+cd creatures
+ls
+cd ..
+ls
+ls -a
+cd data
+ls
+cd ..
+cd data-1
+ls
+cd ..
+ls -F
+cd thesis
+ls
+cd ..
+cd north-pacific-gyre/
+ls
+cd 2012-07-03/
+ls
+nano do-stats.sh
+cat goodiff
+bash goodiff NENE01729A.txt
+cat NENE01729A.txt
+ls
+cat goostats
+bash do-stats.sh *[AB].txt
+nano do-stats.sh
+bash do-stats.sh *[AB].txt
+cd ..
+ls
+cd ..
+ls
+cd writing
+ls
+cat haiku.txt
+grep not haiku.txt
+grep The haiku.txt
+grep -w "is not" haiku.txt
+grep -n "it" haiku.txt
+grep -n -w -i "the" haiku.txt
+grep -n -w "the" haiku.txt
+grep -n -w -v "the" haiku.txt
+grep -E '^.o' haiku.txt
+grep -E '^..o' haiku.txt
+grep -E '^..e' haiku.txt
+grep -E '^....T' haiku.txt
+pwd
+ls
+find .
+ls -a
+ls -aF
+find . -type d
+find . -type f
+find . -name *.txt
+find . -name haiku.txt
+find . -name '*.txt'
+wc -l $(find . -name '*.txt')
+grep "FE" $(find .. -name '*.pdb')
+cd ~
+git config --global user.name renjianning
+git config --global user.email renjianning@gmail.com
+git config --global color.ui "auto"
+git config --global core.editor "atom --wait"
+git config --list
+git config --global http.proxy proxy-url
+git config -h
+git config --help
+ls
+mkdir planets
+cd planets
+git init
+ls
+ls -a
+git status
+nano mars.txt
+ls
+cat mars.txt
+git status
+git add mars.txt
+git status
+git commit -m " start nots on mars as a base"
+ls .
+ls 
+ls -a
+cd .git
+ls
+git status
+cd ..
+ls
+git status
+git log
+nano mars.txt
+git status
+git diff
+git commit -m "add concerns about effects of mars"
+git commit -m "add concerns about effects of mars"
+git status
+git add mars.txt
+git commit -m "add concerns about effects of mars"
+git diff
+cat mars.txt
+nano mars.txt
+git diff
+git add mars.txt
+git diff
+git diff --staged
+git commit -m "discuss mummyu"
+git status
+git status --staged
+git diff --staged
+git status
+git log
+git diff
+git diff mars.txt
+git diff HEAD mars.txt
+nano mars.txt
+git diff
+git diff HEAD mars.txt
+git diff HEAD~1 mars.txt
+git diff HEAD~2 mars.txt
+git show HEAD~2 mars.txt
+nano mars.txt
+git status
+git checkout HEAD mars.txt
+cat mars.txt
+git log
+git checkout da525 mars.txt
+cat mars.txt
+git status
+git checkout of master mars.txt
+git checkout -f master mars.txt
+git checkout f22b25e mars.txt
+git status
+mkdir results
+touch a.dat b.dat c.dat results/a.out results/b.out
+git status
+ls
+cd results
+cat a.dat
+nano a.dat
+nano mars.txt
+nano b.dat
+cd results
+ls
+pwe
+pwd
+nano a.out
+man touch
+nano .gitignore
+cat .gitignore
+pwd
+cd ..
+ls -a
+nano .gitignore
+cat .gitignore
+cat .gitignore
+nano .gitignore
+ls
+ls -a
+cat .gitignore
+git status
+git add .gitignore
+git commit -m "add the ignore file"
+git status
+git add a.dat
+git add -f a.dat
+git status --ignored
+git remote add origin https://github.com/renjianning/planets.git
+git remote -v
+git push origin master
+ls
+git push origin master
+git config --global http.proxy http://user:password@proxy.url
+git push origin master
+git config --global --unset http.proxy
+git push origin master
+unset SSH_ASKPASS
+git push -u origin master
+git pull origin master
+history
+history | tial -201 | colrm 1 7 git_config.sh
+history | tail -201 | colrm 1 7 git_config.sh
+history | tail -201 > | colrm 1 7 git_config.sh
+history | tail -201  | colrm 1 7 > git_config.sh
